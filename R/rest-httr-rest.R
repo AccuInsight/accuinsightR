2512b278b1955a406a1804de4a42664349846623
Hyunjoong Kim <hj.ben.kim@gmail.com>
2021-07-26 14:22:49 +0900

    add workspace run code

--- R/rest-httr-rest.R
+++ R/rest-httr-rest.R
@@ -12,6 +12,18 @@ accu_rest_path <- function(version, env_value) {
   )
 }
 
+accu_ws_run_rest_path <- function(version, env_value, mode) {
+  const_val <- accu.consts
+  
+  projectID <- env_value[[const_val$ENV_PROJECT_ID]]
+  workspaceID <- env_value[[const_val$ENV_WORKSPACE_ID]]
+  endpoint <- sprintf("project/%s/workspace/%s/%s",projectID, workspaceID, mode)
+  switch(
+    version,
+    "1.0" = endpoint
+  )
+}
+
 #' @importFrom httr timeout
 accu_rest_timeout <- function() {
   httr::timeout(getOption("accu.rest.timeout", 60))
@@ -109,5 +121,68 @@ accu_rest <- function( ..., client, query = NULL, data = NULL, env_value = NULL,
     stop(msg, call. = FALSE)
   }
   text <- httr::content(response, "text", encoding = "UTF-8")
+  print("-- response text --")
+  print(text)
+  jsonlite::fromJSON(text, simplifyVector = FALSE)
+}
+
+#' @importFrom httr GET POST add_headers config content
+accu_ws_run_rest <- function( ..., mode, client, query = NULL, data = NULL, env_value = NULL, verb = "GET", version = "1.0",
+                       max_rate_limit_interval=60) {
+  host_creds <- client$get_host_creds()
+  rest_config <- get_rest_config(host_creds)
+  args <- list(...)
+  api_url <- paste0(host_creds$host, accu_ws_run_rest_path(version, env_value, mode))
+  req_headers <- do.call(httr::add_headers, rest_config$headers)
+  
+  get_response <- switch(
+    verb,
+    GET = function() {
+      httr::GET( api_url, query = query, accu_rest_timeout(), config = rest_config$config,
+                 req_headers)
+    },
+    POST = function(){
+      httr::POST( api_url,
+                  body = if (is.null(data)) NULL else rapply(data, as.character, how = "replace"),
+                  encode = "json",
+                  accu_rest_timeout(),
+                  config = rest_config$config,
+                  req_headers
+      )
+    },
+    stop("Verb '", verb, "' is unsupported.", call. = FALSE)
+  )
+  sleep_for <- 1
+  time_left <- max_rate_limit_interval
+  response <- get_response()
+  while (response$status_code == 429 && time_left > 0) {
+    time_left <- time_left - sleep_for
+    warning(paste("Request returned with status code 429 (Rate limit exceeded). Retrying after ",
+                  sleep_for, " seconds. Will continue to retry 429s for up to ", time_left,
+                  " second.", sep = ""))
+    Sys.sleep(sleep_for)
+    sleep_for <- min(time_left, sleep_for * 2)
+    response <- get_response()
+  }
+  if (response$status_code != 200) {
+    message_body <- tryCatch(
+      paste(httr::content(response, "parsed", type = "application/json"), collapse = "; "),
+      error = function(e) {
+        try_parse_response_as_text(response)
+      }
+    )
+    msg <- paste("API request to endpoint '",
+                 paste(args, collapse = "/"),
+                 "' failed with error code ",
+                 response$status_code,
+                 ". Reponse body: '",
+                 message_body,
+                 "'",
+                 sep = "")
+    stop(msg, call. = FALSE)
+  }
+  text <- httr::content(response, "text", encoding = "UTF-8")
+  print("-- response text --")
+  print(text)
   jsonlite::fromJSON(text, simplifyVector = FALSE)
 }
