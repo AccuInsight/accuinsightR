6f285f8f8f426ce33da0e68a89369dddeef16ed4
Hyunjoong Kim <hj.ben.kim@gmail.com>
2021-08-27 18:46:04 +0900

    Update workspace-run.R
    regex error fixed

--- R/workspace-run.R
+++ R/workspace-run.R
@@ -24,13 +24,10 @@ accu_workspace_run <- function(client = NULL) {
   argv <- parser$parse_args()
   
   if (is.null(argv$args)) {
-    library(stringr)
-    args <- str_replace_all(str_replace_all(str_replace_all(argv$args, '[["hyphen"]]', ''), '[[:equal:]]', '='), '[[:space:]]', ' ')
+    args <- gsub('\\[\\[:hyphen:\\]\\]', '', gsub('\\[\\[:equal:\\]\\]', '=', gsub('\\[\\[:space:\\]\\]', ' ', argv$args)))
   } else {
     args <- ''
   }
-  
-  print(args)
   
   library(subprocess)
   handle <- spawn_process('/usr/local/bin/R', c('CMD', 'BATCH', paste0('--args', args), argv$codePath, paste0('/tmp/output_', argv$workspaceRunId, '.log')))
