549a931696e607fa5b45806d2bca8bcd437b8597
Hyunjoong Kim <hj.ben.kim@gmail.com>
2021-11-03 15:46:49 +0900

    Update rest-experiments.R
    json path error fix

--- R/rest-experiments.R
+++ R/rest-experiments.R
@@ -111,10 +111,13 @@ accu_create_experiment <- function(artifact_location = NULL, client = NULL) {
   accu_set_current_run()
   run_meta = accu_get_current_run()
   
+  result_path = const_val$RUN_INFO_RESULT_PATH
+  model_json_path = const_val$RUN_INFO_MODEL_JSON_PATH
+  
   print(run_meta)
-  print(run_meta$(const_val$RUN_INFO_RESULT_PATH)$(const_val$RUN_INFO_MODEL_JSON_PATH))
+  print(run_meta$result_path$model_json_path)
   user_id = env_value[[const_val$ENV_USER_SSO_ID]]
-  model_json = jsonlite::fromJSON(txt=run_meta$(const_val$RUN_INFO_RESULT_PATH)$(const_val$RUN_INFO_MODEL_JSON_PATH))
+  model_json = jsonlite::fromJSON(txt=run_meta$result_path$model_json_path)
   
   print(model_json)
   print(model_json$user_id)
