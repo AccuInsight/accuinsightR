02ac468982d594742ebd6fa6d8058d64435859ce
Hyunjoong Kim <hj.ben.kim@gmail.com>
2022-01-13 14:05:45 +0900

    Update rest-globals.R

--- R/rest-globals.R
+++ R/rest-globals.R
@@ -8,7 +8,7 @@
 #' @export
 accu_set_tracking_uri <- function() {
   constVal <- accu.consts
-  # print(constVal$BACK_END_API_URL)
+  print(constVal$BACK_END_API_URL)
   .globals$tracking_uri <- constVal$BACK_END_API_URL
   #invisible(uri)
 }
@@ -22,11 +22,14 @@ accu_get_tracking_uri <- function() {
   .globals$tracking_uri
 }
 
-accu_set_current_run <- function () {
-  .globals$run_info_json = get_current_run()
+accu_set_current_run <- function (run_meta) {
+  .globals$run_info_json = run_meta
 }
 
 accu_get_current_run <- function () {
   .globals$run_info_json
 }
 
+accu_get_current_run_id <- function () {
+  .globals$run_info_json$run_name
+}
