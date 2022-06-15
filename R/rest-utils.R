7de736eca39dba756152ecad9de6340e88dee5c4
Ben <hj.ben.kim@gmail.com>
2022-06-16 08:40:40 +0900

    load backend api info from pod env

--- R/rest-utils.R
+++ R/rest-utils.R
@@ -4,7 +4,9 @@ get_object_keys <- function (obj) {
 
 resolve_client <- function(client) {
   if (is.null(client)) {
-    restUrl <- accu.consts$BACK_END_API_URL
+    env_value = get_os_env('ENV')
+    restUrl <- env_value[[accu.consts$ENV_BACK_END_API_URL]]
+    # restUrl <- accu.consts$BACK_END_API_URL
     accu_client(tracking_uri = restUrl)
   } else {
     if (!inherits(client, "accu_client")) stop("`client` must be an `accu_client` object.", call. = FALSE)
